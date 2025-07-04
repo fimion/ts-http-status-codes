7ed9c330f222647b183d333809452a9524ab002e
Alex Riviere <fimion@gmail.com>
2025-07-03 22:37:56 -0400

    add loose types

--- src/5xx.ts
+++ src/5xx.ts
@@ -1,12 +1,14 @@
+import type { CreateArrayWithLengthX, NumericRange } from "./common.ts";
+
 export type SERVER_ERROR_CODES =
   | 500 /** Internal Server Error */
   | 501 /** Not Implemented */
   | 502 /** Bad Gateway */
   | 503 /** Service Unavailable */
   | 504 /** Gateway Timeout */
-  | 505 /** HTTP Version Not Supported */;
+  | 505 /** "HTTP Version Not Supported" */;
 
-export const CODES_5XX: Record<string, SERVER_ERROR_CODES> = {
+export const CODES_5XX = {
   INTERNAL_SERVER_ERROR: 500,
   NOT_IMPLEMENTED: 501,
   BAD_GATEWAY: 502,
@@ -15,7 +17,7 @@ export const CODES_5XX: Record<string, SERVER_ERROR_CODES> = {
   HTTP_VERSION_NOT_SUPPORTED: 505,
 } as const;
 
-export const STATUSES_5XX: Record<SERVER_ERROR_CODES, string> = {
+export const STATUSES_5XX = {
   500: "INTERNAL_SERVER_ERROR",
   501: "NOT_IMPLEMENTED",
   502: "BAD_GATEWAY",
@@ -27,4 +29,15 @@ export const STATUSES_5XX: Record<SERVER_ERROR_CODES, string> = {
 export function isStrict5xx(value: unknown): value is SERVER_ERROR_CODES {
   if (typeof value !== "number") return false;
   return value in STATUSES_5XX;
+}
+
+export type LOOSE_SERVER_ERROR_CODES = NumericRange<
+  CreateArrayWithLengthX<500>,
+  599
+>;
+
+export function is5xx(value: unknown): value is LOOSE_SERVER_ERROR_CODES {
+  if (typeof value !== "number") return false;
+  if (!Number.isInteger(value)) return false;
+  return value >= 500 && value <= 599;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,20 +1,38 @@
-import { describe, expect, it } from "vitest";
-import { welcome, isStrict5xx } from "../src";
+import { describe, expect, assertType, it } from "vitest";
+import {
+  welcome,
+  isStrict5xx,
+  is5xx,
+  CODES_5XX,
+  type LOOSE_SERVER_ERROR_CODES,
+} from "../src";
 
 describe("@fimion/ts-http-status-codes", () => {
   it("works", () => {
     expect(welcome()).toMatchInlineSnapshot('"hello world"');
   });
   it.each([
-    { code: 500, value: true },
-    { code: 501, value: true },
-    { code: 502, value: true },
-    { code: 503, value: true },
-    { code: 504, value: true },
-    { code: 505, value: true },
+    { code: CODES_5XX.INTERNAL_SERVER_ERROR, value: true },
+    { code: CODES_5XX.NOT_IMPLEMENTED, value: true },
+    { code: CODES_5XX.BAD_GATEWAY, value: true },
+    { code: CODES_5XX.SERVICE_UNAVAILABLE, value: true },
+    { code: CODES_5XX.GATEWAY_TIMEOUT, value: true },
+    { code: CODES_5XX.HTTP_VERSION_NOT_SUPPORTED, value: true },
     { code: 506, value: false },
     { code: 400, value: false },
   ])("isStrict5xx($code) === $value", ({ code, value }) => {
     expect(isStrict5xx(code)).toBe(value);
+  });
+
+  it("is5xx is only 500s", () => {
+    expect(is5xx(499)).toBe(false);
+    expect(is5xx(600)).toBe(false);
+    for (let x = 500; x < 600; x++) {
+      expect(is5xx(x)).toBe(true);
+    }
+  });
+
+  it("LOOSE_SERVER_ERROR_CODES are correctly typed", () => {
+    assertType<LOOSE_SERVER_ERROR_CODES>(CODES_5XX.INTERNAL_SERVER_ERROR);
   });
 });
